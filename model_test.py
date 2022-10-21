5324fec7e6485b56c057848079248451ccf7ff87
MURAKI Taichi <muraki.taichi@gmail.com>
2022-10-21 18:40:24 +0900

    fix

--- model_test.py
+++ model_test.py
@@ -23,7 +23,7 @@ def test_for_simple_model():
     print(outputs.shape)
 
 
-def visualize(image, silhouettes):
+def visualize(image, silhouettes, mask):
     # print(silhouettes)
     fig, axs = plt.subplots(2, 2)
     axs = axs.flatten()
@@ -31,11 +31,16 @@ def visualize(image, silhouettes):
     axs[0].imshow(image[0].permute(1, 2, 0).detach().numpy())
     axs[1].set_title("silhouettes")
     axs[1].imshow(silhouettes[0].detach().numpy())
+    axs[2].set_title("silhouettes")
+    axs[2].imshow(mask.detach().numpy())
     plt.show()
 
 
 def load_image():
     image_name = "data/input_images/datasets/training/images/image_000000.jpg"
+    mask_name = "data/input_images/datasets/training/masks/image_000000.png"
+    mask = cv2.imread(mask_name, cv2.IMREAD_GRAYSCALE)
+
     orig_image = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
     image = 255 - orig_image
 
@@ -56,7 +61,7 @@ def load_image():
     im_rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     im_rgb = torch.from_numpy(im_rgb.transpose(2, 0, 1)).clone()
     im_rgb = im_rgb.unsqueeze(0) / 255
-    return im_rgb, image_ref
+    return im_rgb, image_ref, torch.from_numpy(mask)
 
 
 def test_for_full_simple_model():
@@ -68,7 +73,8 @@ def test_for_full_simple_model():
     model = HandSilhouetteNet3(model_path, num_pca_comps, device)
 
     # image_name = "data/input_images/datasets/training/images/image_000000.jpg"
-    inputs, image_refs = load_image()
+    inputs, image_refs, mask = load_image()
+    print(f"mask: dtype={mask.dtype} shape={mask.shape}")
 
     print(inputs.dtype, image_refs.dtype)
     print(inputs.shape, image_refs.shape)
@@ -80,7 +86,7 @@ def test_for_full_simple_model():
     silhouettes = outputs["silhouettes"]
     # loss = criterion(outputs, image_refs, labels, dist_maps, meshes, device)
     print(outputs.keys())
-    visualize(inputs, silhouettes)
+    visualize(inputs, silhouettes, mask)
 
 
 if __name__ == "__main__":
