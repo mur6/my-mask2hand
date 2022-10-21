10c45163f3eb3399386c1927cd0903aaba5c06f3
MURAKI Taichi <muraki.taichi@gmail.com>
2022-10-21 15:51:30 +0900

    fix

--- model_test.py
+++ model_test.py
@@ -19,5 +19,23 @@ def test_for_simple_model():
     print(outputs.shape)
 
 
+def test_for_full_simple_model():
+    from model2 import HandSilhouetteNet3
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model_path = "./models/MANO_RIGHT.pkl"
+    num_pca_comps = 45
+    model = HandSilhouetteNet3(model_path, num_pca_comps, device)
+
+    # # x = torch.rand(1, 512)
+    # # print(x.shape, focal_lens.shape)
+    # out = model(, focal_lens)
+    inputs = torch.rand(1, 3, 224, 224)
+    focal_lens = torch.tensor([[531.9495, 532.2600]])
+    image_refs = torch.rand(1, 224, 224)
+    outputs = model(inputs, focal_lens, image_refs)
+    print(outputs)
+
+
 if __name__ == "__main__":
-    test_for_simple_model()
+    test_for_full_simple_model()
