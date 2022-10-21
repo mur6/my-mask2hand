b6710d8da605460e721155ce4ac95850b24b76cc
MURAKI Taichi <muraki.taichi@gmail.com>
2022-10-21 17:40:02 +0900

    可視化を追加

--- model_test.py
+++ model_test.py
@@ -1,6 +1,9 @@
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+from loss import criterion
 
 
 def test_for_simple_model():
@@ -19,6 +22,15 @@ def test_for_simple_model():
     print(outputs.shape)
 
 
+def visualize(silhouettes):
+    print(silhouettes)
+    fig, axs = plt.subplots(2, 2)
+    axs = axs.flatten()
+    axs[0].set_title("silhouettes")
+    axs[0].imshow(silhouettes[0].detach().numpy())
+    plt.show()
+
+
 def test_for_full_simple_model():
     from model2 import HandSilhouetteNet3
 
@@ -34,7 +46,10 @@ def test_for_full_simple_model():
     focal_lens = torch.tensor([[531.9495, 532.2600]])
     image_refs = torch.rand(1, 224, 224)
     outputs = model(inputs, focal_lens, image_refs)
-    print(outputs)
+    silhouettes = outputs["silhouettes"]
+    # loss = criterion(outputs, image_refs, labels, dist_maps, meshes, device)
+    print(outputs.keys())
+    visualize(silhouettes)
 
 
 if __name__ == "__main__":
