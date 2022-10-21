0973a2c61af5b24df018b54a9c47dc4eb0f40943
MURAKI Taichi <muraki.taichi@gmail.com>
2022-10-21 15:25:52 +0900

    model2, channel fix

--- model2.py
+++ model2.py
@@ -29,7 +29,7 @@ class Encoder_with_Shape(nn.Module):
         super(Encoder_with_Shape, self).__init__()
 
         self.feature_extractor = models.resnet18(pretrained=True)
-        self.feature_extractor.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.feature_extractor.conv1 = nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         fc_in_features = self.feature_extractor.fc.in_features
         self.feature_extractor.fc = nn.Sequential(nn.Linear(fc_in_features, fc_in_features), nn.ReLU())
 
@@ -237,7 +237,7 @@ def main():
     # # x = torch.rand(1, 512)
     # # print(x.shape, focal_lens.shape)
     # out = model(, focal_lens)
-    inputs = torch.rand(1, 1, 224, 224)
+    inputs = torch.rand(1, 3, 224, 224)
     focal_lens = torch.tensor([[531.9495, 532.2600]])
     image_refs = torch.rand(1, 224, 224)
     outputs = model(inputs, focal_lens, image_refs)
