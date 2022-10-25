d6bed7cf66d705bc34e9a663ee72ea91a66770e4
MURAKI Taichi <muraki.taichi@gmail.com>
2022-10-25 13:38:38 +0900

    render test

--- render_test.py
+++ render_test.py
@@ -31,69 +31,119 @@ from pytorch3d.structures import Meshes
 # from pytorch3d.vis.plotly_vis import AxisArgs, plot_batch_individually, plot_scene
 from pytorch3d.vis.texture_vis import texturesuv_image_matplotlib
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-obj_filename = Path("data/3d/hand.obj")
-
-# Load obj file
-mesh = load_objs_as_meshes([obj_filename], device=device)
-# plt.figure(figsize=(7, 7))
-# texture_image = mesh.textures.maps_padded()
-# plt.imshow(texture_image.squeeze().cpu().numpy())
-# plt.axis("off")
-# plt.show()
-
-# plt.figure(figsize=(7, 7))
-# texturesuv_image_matplotlib(mesh.textures, subsample=None)
-# plt.axis("off")
-# plt.show()
+import mano
 
 
-R, T = look_at_view_transform(2.7, 0, 180)
-cameras = FoVPerspectiveCameras(device=device, R=R, T=T)
+def create_mesh_from_mano(device):
+    mano_model_path = "./models/MANO_RIGHT.pkl"
+    num_pca_comps = 45
+    rh_model = mano.load(model_path=mano_model_path, is_rhand=True, num_pca_comps=num_pca_comps, flat_hand_mean=True)
+    code = torch.rand(1, 61)
+    pose_pcas, global_orient, transl, betas = code[:, :-16], code[:, -16:-13], code[:, -13:-10], code[:, -10:]
+    # Global orient & pose PCAs to 3D hand joints & reconstructed silhouette
+    rh_output = rh_model(
+        betas=betas,
+        global_orient=global_orient,
+        hand_pose=pose_pcas,
+        transl=transl,
+        return_verts=True,
+        return_tips=True,
+    )
 
-# Define the settings for rasterization and shading. Here we set the output image to be of size
-# 512x512. As we are rendering images for visualization purposes only we will set faces_per_pixel=1
-# and blur_radius=0.0. We also set bin_size and max_faces_per_bin to None which ensure that
-# the faster coarse-to-fine rasterization method is used. Refer to rasterize_meshes.py for
-# explanations of these parameters. Refer to docs/notes/renderer.md for an explanation of
-# the difference between naive and coarse-to-fine rasterization.
-raster_settings = RasterizationSettings(
-    image_size=512,
-    blur_radius=0.0,
-    faces_per_pixel=1,
-)
+    # Initialize each vertex to be white in color
+    verts_rgb = torch.ones_like(rh_output.vertices)  # (B, V, 3)
+    textures = TexturesVertex(verts_features=verts_rgb.to(device))
 
-# Place a point light in front of the object. As mentioned above, the front of the cow is facing the
-# -z direction.
-lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
+    # Coordinate transformation from FreiHand to PyTorch3D for rendering
+    # [FreiHand] +X: right, +Y: down, +Z: in
+    # [PyTorch3D] +X: left, +Y: up, +Z: in
+    coordinate_transform = torch.tensor([[-1, -1, 1]]).to(device)
 
-# Create a Phong renderer by composing a rasterizer and a shader. The textured Phong shader will
-# interpolate the texture uv coordinates for each vertex, sample from a texture image and
-# apply the Phong lighting model
-renderer = MeshRenderer(
-    rasterizer=MeshRasterizer(cameras=cameras, raster_settings=raster_settings),
-    shader=SoftPhongShader(device=device, cameras=cameras, lights=lights),
-)
+    mesh_faces = torch.tensor(rh_model.faces.astype(int)).to(device)
 
-# raster_settings = RasterizationSettings(
-#     image_size=224,
-#     blur_radius=0.0,
-#     faces_per_pixel=100,
-# )
-blend_params = BlendParams(sigma=1e-4, gamma=1e-4, background_color=(1.0, 1.0, 1.0))
-silhouette_renderer = MeshRenderer(
-    rasterizer=MeshRasterizer(cameras=cameras, raster_settings=raster_settings),
-    shader=SoftSilhouetteShader(blend_params=blend_params),
-)
+    # Create a Meshes object
+    batch_size = 1
+    hand_meshes = Meshes(
+        verts=[rh_output.vertices[i] * coordinate_transform for i in range(batch_size)],
+        faces=[mesh_faces for i in range(batch_size)],
+        textures=textures,
+    )
+    return hand_meshes
 
-images = renderer(mesh)
-silhouettes = silhouette_renderer(meshes_world=mesh)
-print(f"silhouettes: {silhouettes.shape}")
-silhouettes = silhouettes[0, ..., 3]
-print(f"silhouettes: {silhouettes.shape}")
-plt.figure(figsize=(10, 10))
-plt.imshow(images[0, ..., :3].cpu().numpy())
-# plt.imshow(silhouettes.cpu().numpy())
-plt.axis("off")
-plt.show()
+
+def load_mesh_from_file(device):
+    obj_filename = Path("data/3d/hand.obj")
+    # Load obj file
+    mesh = load_objs_as_meshes([obj_filename], device=device)
+    # plt.figure(figsize=(7, 7))
+    # texture_image = mesh.textures.maps_padded()
+    # plt.imshow(texture_image.squeeze().cpu().numpy())
+    # plt.axis("off")
+    # plt.show()
+
+    # plt.figure(figsize=(7, 7))
+    # texturesuv_image_matplotlib(mesh.textures, subsample=None)
+    # plt.axis("off")
+    # plt.show()
+    return mesh
+
+
+def get_cameras(device):
+    R, T = look_at_view_transform(2.7, 0, 180)
+    cameras = FoVPerspectiveCameras(device=device, R=R, T=T)
+    return cameras
+
+
+def get_renderer(device, cameras):
+    lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
+    raster_settings = RasterizationSettings(
+        image_size=512,
+        blur_radius=0.0,
+        faces_per_pixel=1,
+    )
+    # Create a Phong renderer by composing a rasterizer and a shader. The textured Phong shader will
+    # interpolate the texture uv coordinates for each vertex, sample from a texture image and
+    # apply the Phong lighting model
+    renderer = MeshRenderer(
+        rasterizer=MeshRasterizer(cameras=cameras, raster_settings=raster_settings),
+        shader=SoftPhongShader(device=device, cameras=cameras, lights=lights),
+    )
+    return renderer
+
+
+def get_silhouette_renderer(cameras):
+    raster_settings = RasterizationSettings(
+        image_size=512,
+        blur_radius=0.0,
+        faces_per_pixel=1,
+    )
+    blend_params = BlendParams(sigma=1e-4, gamma=1e-4, background_color=(1.0, 1.0, 1.0))
+    silhouette_renderer = MeshRenderer(
+        rasterizer=MeshRasterizer(cameras=cameras, raster_settings=raster_settings),
+        shader=SoftSilhouetteShader(blend_params=blend_params),
+    )
+    return silhouette_renderer
+
+
+def vizualize(target_tensor):
+    target_np = target_tensor.cpu().numpy()
+    plt.figure(figsize=(10, 10))
+    plt.imshow(target_np)
+    # plt.imshow(silhouettes.cpu().numpy())
+    plt.axis("off")
+    plt.show()
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    cameras = get_cameras(device)
+    mesh = create_mesh_from_mano(device)
+    images = get_renderer(device, cameras)(mesh)
+    silhouettes = get_silhouette_renderer(cameras)(meshes_world=mesh)
+    print(f"silhouettes: {silhouettes.shape}")
+    silhouettes = silhouettes[0, ..., 3]
+    print(f"silhouettes: {silhouettes.shape}")
+    vizualize(images[0, ..., :3])
+
+
+main()
